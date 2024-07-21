c806ed4dc6619b77adf6aa2e256b9b44e7b62af0
MartinRocketScience <154908878+MartinRocketScience@users.noreply.github.com>
2024-07-21 09:08:22 +0200

    Setup correct docstring

--- cypress/support/json_model/sampleModel.js
+++ cypress/support/json_model/sampleModel.js
@@ -1,4 +1,8 @@
 class SampleModel{
+    /**
+     * Checks if the API response body contains the specified title.
+     * @param {string} title - The title to verify in the response body.
+     */
     checkResponseBodyTitle(title) {
         cy.get('@apiRequest').its('body').should('have.property', 'title', title);
     }
